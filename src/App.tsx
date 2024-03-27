dc6420042fa9611a176d4830a5005a249680da29
manishkumarguptadev <manishkumarguptadev@gmail.com>
2024-03-28 00:30:33 +0530

    react-query

--- src/App.tsx
+++ src/App.tsx
@@ -8,9 +8,21 @@ import Users from "./pages/Users";
 import Cabins from "./pages/Cabins";
 import Bookings from "./pages/Bookings";
 import AppLayout from "./ui/AppLayout";
+import { QueryClient, QueryClientProvider } from "@tanstack/react-query";
+import { ReactQueryDevtools } from "@tanstack/react-query-devtools";
+
+const queryClient = new QueryClient({
+  defaultOptions: {
+    queries: {
+      staleTime: 60 * 1000,
+    },
+  },
+});
+
 function App() {
   return (
-    <>
+    <QueryClientProvider client={queryClient}>
+      <ReactQueryDevtools />
       <BrowserRouter>
         <Routes>
           <Route element={<AppLayout />}>
@@ -26,7 +38,7 @@ function App() {
           <Route path="*" element={<PageNotFound />} />
         </Routes>
       </BrowserRouter>
-    </>
+    </QueryClientProvider>
   );
 }
 export default App;
